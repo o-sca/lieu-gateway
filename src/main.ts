df4629b8b9957f366318fd41e201bab9ec1e23c8
Oscar Zhu <ozhu35@gmail.com>
2023-11-26 17:16:11 -0800

    refactor: include user request history

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import * as cors from 'cors';
 import * as express from 'express';
 import * as proxy from 'express-http-proxy';
 import { authMiddleware } from './auth.middleware';
-import { requestTrackerHandler } from './request-tracker.handler';
+import {
+  requestTrackerCreate,
+  requestTrackerInsert,
+  requestTrackerCheck,
+} from './request-tracker.handler';
 
 async function bootstrap() {
   const app = express();
@@ -23,16 +27,41 @@ async function bootstrap() {
   }
 
   app.use(
-    '/api/v1/ai/text',
-    authMiddleware,
-    proxy(process.env.TEXT_AI_URL, {
-      userResDecorator: requestTrackerHandler,
+    '/api/v1/auth',
+    proxy(process.env.AUTH_URL, {
+      userResDecorator: requestTrackerCreate,
     }),
   );
+
+  app.use(
+    '/api/v1/ai/text',
+    authMiddleware,
+    requestTrackerCheck,
+    proxy(process.env.TEXT_AI_URL, {
+      userResDecorator: requestTrackerInsert,
+    }),
+  );
+
   // app.use('/api/v1/ai/gen', authMiddleware, proxy(process.env.GEN_AI_URL));
-  app.use('/api/v1/auth', proxy(process.env.AUTH_URL));
+
+  app.use(
+    '/api/v1/requests',
+    authMiddleware,
+    proxy(process.env.REQUEST_TRACKER_URL, {
+      filter: (req) => {
+        if (req.path === '/create' || req.path === '/insert') {
+          return false;
+        }
+        return true;
+      },
+      proxyReqPathResolver: (req) => {
+        const updatedPath = req.path + '?user_id=' + req['user']['id'];
+        return updatedPath;
+      },
+    }),
+  );
+
   app.use('*', (_req, res) => {
-    return res.status(404);
     return res.status(404).send();
   });
 
